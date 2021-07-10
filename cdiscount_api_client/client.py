47629a0005866b21c1df70909f23786578b0a55d
Steven Athouel <sathouel@gmail.com>
2021-07-10 22:51:07 +0300

    resources added

---
+++ cdiscount_api_client/client.py
@@ -0,0 +1,70 @@
+import requests as rq
+
+from cdiscount_api_client import (
+    resources,
+    utils
+)
+
+class AuthError:
+    pass
+
+class CdiscountClient:
+    BASE_URL = 'https://marketplaceapi.cdiscount.com'
+
+    def __init__(self, client_id, client_secret, subscription_key, seller_id):
+        self._session = rq.Session()
+
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._subscription_key = subscription_key
+        self._seller_id = seller_id
+
+        self._urls = {
+            'token': "https://oauth2.cdiscount.com/auth/realms/maas-international-sellers/protocol/openid-connect/token"
+        }
+        self._resources = {
+            'product_management': resources.ProductManagementPool(
+                utils.urljoin(self.BASE_URL, 'productManagement'), self._session
+            ),
+            'order_management': resources.OrderManagementPool(
+                utils.urljoin(self.BASE_URL, 'OrderManagement/orders'), self._session
+            ),
+            'offer_management': resources.OfferManagementPool(
+                utils.urljoin(self.BASE_URL, 'offerManagement'), self._session
+            )
+        }
+
+        self._authenticate()
+
+    def _authenticate(self):
+        auth_data = {
+            'client_id': self._client_id,
+            'client_secret': self._client_secret,
+            'grant_type': 'client_credentials'
+        }
+        res = self._session.post(self._urls['token'], data=auth_data)
+        if res.status_code != 200:
+            raise AuthError('Error {}: {}'.format(res.status_code, res.text))
+
+        access_token = res.json()['access_token']
+        self._session.headers.update({
+            'Authorization': 'Bearer {}'.format(access_token),
+            'Ocp-Apim-Subscription-Key': self._subscription_key,
+            'SellerId': self._seller_id
+        })
+
+    @property
+    def resources(self):
+        return self._resources
+
+    @property
+    def product_management(self):
+        return self._resources['product_management']
+
+    @property
+    def order_management(self):
+        return self._resources['order_management']
+
+    @property
+    def offer_management(self):
+        return self._resources['offer_management']                
---
+++ cdiscount_api_client/resources.py
@@ -0,0 +1,211 @@
+import json
+
+from cdiscount_api_client.utils import urljoin
+
+class ResourcePool:
+    def __init__(self, endpoint, session):
+        """Initialize the ResourcePool to the given endpoint. Eg: products"""
+        self._endpoint = endpoint
+        self._session = session
+
+    def get_url(self):
+        return self._endpoint
+
+class CreatableResource:
+    def create_item(self, item, files=None):
+        if files:
+            self._session.headers.pop('Content-Type')
+            self._session.headers.pop('Accept')
+            print(self._session.headers)
+            res = self._session.post(self._endpoint, files=files, data=item)
+        else:
+            res = self._session.post(self._endpoint, data=json.dumps(item))
+        return res
+
+class GettableResource:
+    def fetch_item(self, code):
+        url = urljoin(self._endpoint, code)
+        res = self._session.get(url)
+        return res
+
+class ListableResource:
+    def fetch_list(self, args=None):
+        res = self._session.get(self._endpoint, params=args)
+        return res
+
+class SearchableResource:
+    def search(self, query):
+        params = {
+            'query': query
+        }
+        res = self._session.get(self._endpoint, params=params)
+        return res
+
+class UpdatableResource:
+    def update_create_item(self, item, code=None):
+        if code is None:
+            code = item.get('id')
+        url = urljoin(self._endpoint, code)
+        res = self._session.put(url, data=json.dumps(item))
+        return res
+
+class DeletableResource:
+    def delete_item(self, code):
+        url = urljoin(self._endpoint, code)
+        res = self._session.delete(url)
+        return res
+
+# Pools
+
+# Product
+class ProductManagementPool(ResourcePool):
+
+    @property
+    def categories(self):
+        return ProductManagementCatgoriesPool(
+            urljoin(self._endpoint, 'categories'), self._session
+        )
+    
+    @property
+    def models(self):
+        return ProductManagementModelsPool(
+            urljoin(self._endpoint, 'models'), self._session
+        )
+
+    @property
+    def search_products(self):
+        return ProductManagementProductSearchPool(
+            urljoin(self._endpoint, 'products/search',), self._session
+        )
+
+    @property
+    def product_integration_packages(self):
+        return ProductManagementProductIntegrationPool(
+            urljoin(self._endpoint, 'product-integration-packages',), self._session
+        )
+
+class ProductManagementCatgoriesPool(
+    ResourcePool,
+    ListableResource):
+    pass
+
+class ProductManagementModelsPool(
+    ResourcePool,
+    ListableResource):
+    pass
+
+class ProductManagementProductSearchPool(
+    ResourcePool, 
+    CreatableResource):
+    pass
+
+class ProductManagementProductIntegrationPool(
+    ResourcePool,
+    GettableResource,
+    CreatableResource):
+    pass
+    
+# Offer
+
+class OfferManagementPool(ResourcePool):
+    
+    @property
+    def competing_offer_changes(self):
+        return OfferManagementCompetingOfferChangesPool(
+            urljoin(self._endpoint, 'competing-offer-changes'), self._session
+        )
+
+    @property
+    def offers(self):
+        return OfferManagementOffersPool(
+            urljoin(self._endpoint, 'offers'), self._session
+        )
+
+    @property
+    def offer_integration_packages(self):
+        return OfferManagementOfferIntegrationPackagesPool(
+            urljoin(self._endpoint, 'offer-integration-packages'), self._session
+        )
+
+    @property
+    def seller_deals_search(self):
+        return OfferManagementSellerDealsSearchPool(
+            urljoin(self._endpoint, 'seller-deals/search'), self._session
+        )        
+
+class OfferManagementCompetingOfferChangesPool(
+    ResourcePool,
+    ListableResource):
+    pass
+
+class OfferManagementOffersPool(
+    ResourcePool,
+    CreatableResource):
+    
+    @property
+    def search(self):
+        return OfferManagementOffersSearchPool(
+            urljoin(self._endpoint, 'search'), self._session
+        )
+
+class OfferManagementOffersSearchPool(
+    ResourcePool,
+    CreatableResource):
+    pass
+
+class OfferManagementOfferIntegrationPackagesPool(
+    ResourcePool,
+    GettableResource,
+    CreatableResource):
+    pass
+
+class OfferManagementSellerDealsSearchPool(
+    ResourcePool,
+    CreatableResource):
+    pass
+
+# Order
+
+class OrderManagementPool(ResourcePool):
+
+    @property
+    def search_orders(self):
+        return OrderManagementSearchOrdersPool(
+            urljoin(self._endpoint, 'search'), self._session
+        )    
+    
+    def commercial_gestures(self, order_number):
+        return OrderManagementCommercialGesturesPool(
+            urljoin(self._endpoint, order_number, 'commercial-gestures'), self._session
+        )
+
+    def commercial_gesture_eligibilities(self, order_number):
+        return OrderManagementCommercialGestureEligibilitiesPool(
+            urljoin(self._endpoint, order_number, 'commercial-gesture-eligibilities'), self._session
+        )
+
+    def validate_order(self, order_number):
+        return OrderManagementValidateOrdersPool(
+            urljoin(self._endpoint, order_number, 'validate'), self._session
+        )
+
+class OrderManagementCommercialGesturesPool(
+    ResourcePool, 
+    CreatableResource,
+    ListableResource):
+    pass
+
+class OrderManagementCommercialGestureEligibilitiesPool(
+    ResourcePool,
+    ListableResource):
+    pass
+
+class OrderManagementSearchOrdersPool(
+    ResourcePool,
+    CreatableResource):
+    pass
+
+class OrderManagementValidateOrdersPool(
+    ResourcePool,
+    UpdatableResource):
+    pass
